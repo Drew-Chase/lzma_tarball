73fbd75a34a5e9178b97c51ae4efec80bdcc10f1
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-11-14 16:16:58 -0500

    Refactor `main.rs` to improve compression setup.
    Reorganized the `Main.rs` to make the compression setup clearer and more modular. Introduced new methods for setting paths, directories, and files, and updated existing methods to reflect their new names. This enhances readability and maintainability of the compression configuration.

--- examples/compress_path/src/main.rs
+++ examples/compress_path/src/main.rs
@@ -1,24 +1,47 @@
-use lzma_tarball::writer::LZMATarballWriter;
+use lzma_tarball::writer::{ArchiveEntry, LZMATarballWriter};
+use std::path::PathBuf;
 
 fn main() {
-	// this can be any directory or file, relative or absolute path
-	let input_path = "./";
-
-	// this is the output file.
-	// this will create the parent directories if they don't exist.
-	let output = "../test/test.tar.xz";
-
-	let result = LZMATarballWriter::new(input_path, output)
-		.unwrap()
+	let result = LZMATarballWriter::new()
 		// Set the compression level to 6 - this is the default
 		// the range is 0-9, where 0 is no compression and 9 is maximum compression
-		.with_compression_level(6)
+		.set_compression_level(6)
 		// Set the buffer size to 64 - this is the default
 		// this is the size of the buffer used to read and write data
 		// the larger, the buffer, the faster the compression, but the more memory it uses
 		// the smaller, the buffer, the slower the compression, but the less memory it uses
 		// the buffer size is in kilobytes
-		.with_buffer_size(64)
+		.set_buffer_size(64)
+		// The first argument is the path to the directory or file to compress
+		// the second argument is the path inside the archive
+		// if the second argument is "/", entry will be placed in the root of the archive.
+		.with_path("./", "/all")
+		.unwrap() // This will throw an error if the path does not exist, or it could not be determined if it is a file or directory
+		// Add the contents of the directory to the archive.
+		// The first argument is the path to the directory to compress
+		// the second argument is the path inside the archive
+		.with_directory_contents("./", "/contents")
+		// Filter the contents of the directory.
+		// Only files with a ".rs" extension will be included in the archive.
+		.with_filtered_directory_contents("./", "./rs", &|entry| { entry.path().extension().is_some_and(|ext| ext == "rs") })
+		// Add specific files to the archive.
+		// The first file added is "test.txt", which will appear as "/test.txt" in the archive.
+		// The second file added is "other.txt", which will appear as "/other.txt" in the archive.
+		.with_files(
+			&mut vec![
+				ArchiveEntry {
+					filesystem_path: PathBuf::from("./Cargo.toml"),
+					archive_path: "/individual/Cargo.toml".to_string(),
+				},
+				ArchiveEntry {
+					filesystem_path: PathBuf::from("./Cargo.lock"),
+					archive_path: "/individual/Cargo.lock".to_string(),
+				}
+			]
+		)
+		// this is the output file.
+		// this will create the parent directories if they don't exist.
+		.set_output("../test/test.tar.xz")
 		// Compress the data and report progress
 		.compress(|progress| {
 			// The percentage is between 0.0 and 1.0
